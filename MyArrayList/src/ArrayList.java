19ae7052ddda8625cc3bc956d0814fe2beda9099
mudita srivastava <muditasrivastava2020@gmail.com>
2021-08-05 21:58:21 +0530

    First Commit on ArrayList

---
+++ MyArrayList/src/ArrayList.java
@@ -0,0 +1,13 @@
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		pair <String,Integer> p1 = new pair ("Mudita", 857);
+		pair <Boolean,String> p2 = new pair(true, "hello");
+		p1.getDescription();
+		p2.getDescription();
+		
+
+	}
+
+}
---
+++ MyArrayList/src/pair.java
@@ -0,0 +1,15 @@
+public class pair<X,Y> {
+	X x;
+	Y y;
+	public pair (X x, Y y) {
+		this.x = x;
+		this.y =y;
+	}
+
+	public void getDescription() {
+			System.out.println(x + " and " + y);
+			
+			
+	}
+
+}
